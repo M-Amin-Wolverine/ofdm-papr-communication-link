e8e2a210b9a05120f7b1cb13d97ddf00db714a93
Uncle Zeynoth <amin.khodadadi5246@gmail.com>
2026-09-01 07:41:28 -0700

    Update test_crc.py

--- tests/unit/test_crc.py
+++ tests/unit/test_crc.py
@@ -1,1 +1,11 @@
+import numpy as np
+import pytest
 
+from ofdm_linksim.crc import encode_crc, check_crc
+
+
+def test_crc_encode_check():
+    data = np.array([1, 0, 1, 1], dtype=bool)
+    crc32 = encode_crc(data)
+    assert len(crc32) == 32
+    assert check_crc(data, crc32) is True
